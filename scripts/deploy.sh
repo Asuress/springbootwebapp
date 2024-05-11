f2946be9db58d4eb03e1a9dff2ee9842e848991a
Maxim Kryuchkov <maxim.kryuchkov1@gmail.com>
2024-05-11 21:52:21 +0300

    deploy script changes

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,23 +1,25 @@
 #!/usr/bin/env bash
 
-APP_NAME="sweater-1.0-SNAPSHOT.jar"
-SSH_USER_ADDRESS="maxim@95.140.158.3"
+# APP_NAME="sweater-1.0-SNAPSHOT.jar"
+# SSH_USER_ADDRESS="maxim@95.140.158.3"
+
+echo 'Packaging files...'
 
 mvn clean package
 
-echo 'Copy files...'
+# echo 'Copy files...'
 
-scp -i ~/.ssh/id_rsa \
-    target/$APP_NAME \
-    $SSH_USER_ADDRESS:/home/maxim/
+# scp -i ~/.ssh/id_rsa \
+#     target/$APP_NAME \
+#     $SSH_USER_ADDRESS:/home/maxim/
 
 echo 'Restart server...'
 
-ssh -i ~/.ssh/id_rsa $SSH_USER_ADDRESS << EOF
+# ssh -i ~/.ssh/id_rsa $SSH_USER_ADDRESS << EOF
 
 pgrep java | xargs kill -9
 nohup java -jar $APP_NAME > log.txt &
 
-EOF
+# EOF
 
 echo 'Bye'
